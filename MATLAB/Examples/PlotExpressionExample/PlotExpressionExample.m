05bbe49d308dec3eb7d9794c27b4d49470d1b507
Ali7403 <85498043+Ali7403@users.noreply.github.com>
2025-04-06 18:23:30 +0200

    Delete MATLAB/Examples directory

--- MATLAB/Examples/PlotExpressionExample/PlotExpressionExample.m
+++
@@ -1,4 +0,0 @@
-%% Plot Expression
-% Plot |sin(x)| over the default |x| interval |[-5 5]|.
-
-fplot(@(x) sin(x))
--- MATLAB/Examples/PlotParametricCurveExample/PlotParametricCurveExample.m
+++
@@ -1,6 +0,0 @@
-%% Plot Parametric Curve
-% Plot the parametric curve $x = cos(3t)$ and $y = sin(2t)$.
-
-xt = @(t) cos(3*t);
-yt = @(t) sin(2*t);
-fplot(xt,yt)
